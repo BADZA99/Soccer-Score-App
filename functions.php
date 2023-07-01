c05e0677b474763b18a9a53627b6ba99cb1b45af
papabn <pndiaye999@gmail.com>
2023-07-01 19:17:31 +0000

    fix error and finalisation

--- functions.php
+++ functions.php
@@ -28,5 +28,28 @@ function convertIntoArray(){
     return $resultArray;
 }
 
+function yesterdayMatches(){
+    $date = date('Y-m-d');
+    $yesterday=strftime("%Y-%m-%d",strftime("date -1 day"));
+    $url = "https://api.soccersapi.com/v2.2/fixtures/?user=pndiaye999&token=d3a3a946de61601e4435f133c7d48e3b&t=schedule&d=.$date.&league_id=1609";
+    // init call
+    $curl = curl_init($url);
+
+    // options GET request
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    // send request
+    $result = curl_exec($curl);
+
+    // close connection
+    curl_close($curl);
+
+    return json_encode($result,true);
+}
+
 
 ?>
--- index.php
+++ index.php
@@ -9,25 +9,34 @@
 
 <div class="container-lg main">
     <div class="row mt-4">
-        <?php foreach ($responseArray['data'] as $object) { ?>
-            <div class="col-lg-4  col-md-12 col-sm-12 text-center mb-3">
-                <div class="card signle-card">
-                    <img src="" alt="" class="sinle-card-img">
-                    <div class="card-body">
-                        <div class="card-title" style="font-weight: bold;"></div>
-                        <div class="card-text single-card-text">
-                            <img  style="width: 50px"src="<?php echo $object['teams']['home']['img'] ?>" alt="">
-                            <?php echo $object['teams']['home']['name'] ?>
-                            <?php echo $object['scores']['home_score'] ?>
-                            -
-                            <img  style="width: 50px"src="<?php echo $object['teams']['away']['img'] ?>" alt="">
-                            <?php echo $object['scores']['away_score'] ?>
-                            <?php echo $object['teams']['away']['name'] ?>
+        <?php if (array_key_exists('data', $responseArray)) { ?>
+
+            <?php foreach ($responseArray['data'] as $object) { ?>
+                <div class="col-lg-4  col-md-12 col-sm-12 text-center mb-3">
+                    <div class="card signle-card">
+                        <img src="" alt="" class="sinle-card-img">
+                        <div class="card-body">
+                            <div class="card-title" style="font-weight: bold;"></div>
+                            <div class="card-text single-card-text">
+                                <img style="width: 50px" src="<?php echo $object['teams']['home']['img'] ?>" alt="">
+                                <?php echo $object['teams']['home']['name'] ?>
+                                <?php echo $object['scores']['home_score'] ?>
+                                -
+                                <img style="width: 50px" src="<?php echo $object['teams']['away']['img'] ?>" alt="">
+                                <?php echo $object['scores']['away_score'] ?>
+                                <?php echo $object['teams']['away']['name'] ?>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            <?php } ?>
+
+        <?php } else { ?>
+
+            <h3 class="text-center">There are no match today</h3>
+
         <?php } ?>
+
     </div>
 </div>
 
