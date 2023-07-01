2b7fdd6864eed09df810ad8182fe8f4cbc368ba8
papabn <pndiaye999@gmail.com>
2023-07-01 17:51:25 +0000

    setup project and file

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include_once('header.php');?>
+
+
+
+
+
+<div class="container-lg main">
+    <div class="row mt-4">
+        <div class="col-lg-3  col-md-12 col-sm-12 text-center mb-3">
+            <div class="card signle-card">
+                <img src="" alt="" class="sinle-card-img">
+                <div class="card-body">
+                    <div class="card-title">Title</div>
+                    <div class="card-text single-card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include_once('footer.php');?>
+
+
